533d6a03bdcabd63573df00ec6272fc402ad1375
전은형 <ooooo5h91@gmail.com>
2021-07-06 22:35:45 +0900

    임시로 741을 문제 숫자로 지정

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -6,6 +6,16 @@ public class MainDrive {
 		
 //		숫자 야구 게임을 만들어보자
 		
+//		1.임시로 3자리 숫자를 3칸짜리 배열에 나눠서 저장 ex)741
+		
+		int[] randomNumbers = new int[3];
+		
+		randomNumbers[0] = 7;
+		randomNumbers[1] = 4;
+		randomNumbers[2] = 1;
+		
+		
+		
 		
 	}
 
